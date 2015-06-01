4089cae85e5817dfb080ec7a9eeefd9951ed00f9
godofwar91 <davidebernardini91@gmail.com>
2015-06-01 18:04:01 +0200

    aggiunto il main, mapper, reducer e modificato il pom

---
+++ GendataInterpretation/src/main/java/main/AttributeCount.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.io.IOException;
+
+import mapper.AttributeCountMapper;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import reducer.AttributeCountReducer;
+
+public class AttributeCount {
+	public static void main(String[] args) throws IOException,
+	InterruptedException, ClassNotFoundException {
+		Path input = new Path(args[0]);
+		// Path temp1 = new Path(args[1]);
+		Path output = new Path(args[1]);
+
+		Job job = new Job(new Configuration(), "COUNT ATTRIBUTE");
+
+		FileSystem fs = FileSystem.get(job.getConfiguration());
+		// get the FileStatus list from given dir
+		FileStatus[] status_list = fs.listStatus(input);
+		if (status_list != null) {
+			for (FileStatus status : status_list) {
+				// add each file to the list of inputs for the map-reduce job
+				FileInputFormat.addInputPath(job, status.getPath());
+			}
+		}
+
+		FileOutputFormat.setOutputPath(job, output);
+		job.setJarByClass(AttributeCount.class);
+
+		job.setMapperClass(AttributeCountMapper.class);
+		job.setCombinerClass(AttributeCountReducer.class);
+		job.setReducerClass(AttributeCountReducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		job.waitForCompletion(true);
+
+	}
+}
---
+++ GendataInterpretation/src/main/java/reducer/AttributeCountReducer.java
@@ -0,0 +1,28 @@
+package reducer;
+
+import java.io.IOException;
+
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+
+
+
+public class AttributeCountReducer extends  Reducer<Text, LongWritable, Text, LongWritable> {
+
+
+
+	public void reduce(Text key, Iterable<LongWritable> values, Context context)
+			throws IOException, InterruptedException {
+		long sum = 0;
+		for (LongWritable value : values) {
+			sum += value.get();
+		}
+		context.write(key, new LongWritable(sum));
+	}
+
+}
